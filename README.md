53ceb9bd2ec7ab992fcdd7c942a162ecb93a52eb
Alan Williams <alan.ws.private@gmail.com>
2022-02-16 09:30:18 +0000

    wip

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ https://meritcircle.io/
 https://skymavis.com/
 
 https://www.inboundnow.com/html5-semantic-elements-mean-seo/
+
+https://codepen.io/kevinhufnagl/embed/mdVvvwj?height=608&theme-id=default&default-tab=css%2Cresult&user=kevinhufnagl&slug-hash=mdVvvwj&pen-title=Vercel%20like%20text%20gradient%20transition&name=cp_embed_1
--- components/headers.tsx
+++ components/headers.tsx
@@ -1,23 +1,65 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, keyframes, Text } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
 import { Container } from "./containers";
 
+// {
+//     background: "-webkit-linear-gradient(#eee, #333)",
+//     "-webkit-background-clip": "text",
+//     "-webkit-text-fill-color": "transparent",
+//   }
+
+const colorChange = keyframes`
+  0%, 100% { opacity: 0; }
+  50% { opacity: 1; }
+`;
+
 export const Header = () => {
+  const play = useRef<HTMLParagraphElement>(null);
+  const earn = useRef<HTMLParagraphElement>(null);
+  const repeat = useRef<HTMLParagraphElement>(null);
+
   return (
     <Container>
       <Flex as="header" flexDirection={"column"} alignItems="center">
         <Text
           as="h1"
           textStyle="Statment"
-          __css={{
-            fontSize: "500px",
-          }}
+          userSelect={"none"}
+          sx={{ content: "'Play.'", padding: "0.05em" }}
         >
-          Play.
+          <Text
+            as="span"
+            _before={{
+              content: "'Play.'",
+              position: "absolute",
+              display: "block",
+              width: "100%",
+              textAlign: "center",
+              color: "black",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              zIndex: 0,
+              paddingLeft: "0.05em",
+              paddingRight: "0.05em",
+              animation: `${colorChange} 8s infinite`,
+            }}
+            sx={{
+              position: "relative",
+              zIndex: 1,
+              background: "linear-gradient(90deg,#eee, #333)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+            animation={`${colorChange} 8s infinite`}
+          >
+            Play.
+          </Text>
         </Text>
-        <Text as="h1" textStyle="Statment">
+        <Text ref={earn} as="h1" textStyle="Statment">
           Earn.
         </Text>
-        <Text as="h1" textStyle="Statment">
+        <Text ref={repeat} as="h1" textStyle="Statment">
           Repeat.
         </Text>
       </Flex>
